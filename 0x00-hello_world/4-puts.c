64e18d59cc5a932f948856dfba4df71a4cd0c571
loop hole <loop_denver@gmail>
2023-06-15 12:47:34 -0700

    update..V5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main -prints "Programming is like building a multilingual puzzle"
- * return: Always 0.
+ * main -prints "Programming is like building a multilingual puzzle
+ * Return: Always 0.
  */
 
 
@@ -11,3 +11,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
