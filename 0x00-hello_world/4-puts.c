6fdab99348cdfc2eb88498e677373c2f79355bb3
loop hole <loop_denver@gmail>
2023-06-15 12:23:32 -0700

    print exactly as without using printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -prints "programming is like building a multilingual puzzle"
+ * return 0.
+ */
+
+int main(viod)
+
+{
+	puts ("\"programming is like building a multiligual puzzle");
+	
+	return (0);
+
+}
