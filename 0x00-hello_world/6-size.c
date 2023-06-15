cdcb29d4155df5c0a49f46a35b2cdb10819efafc
loop hole <loop_denver@gmail>
2023-06-15 14:49:11 -0700

    print size of various types_v3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Prints the size of various types based on
- * the computer it is compiled and run on the computer it is compiled and run on..
+ * the computer it is compiled and run on..
  * Return: Always 0.
  */
 
